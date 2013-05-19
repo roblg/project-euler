9cc93501d71d9091ce4ca23110a4f73770132191
Robert Gay <robert.gay@redfin.com>
2013-05-18 23:33:38 -0700

    node.js solution to Problem 4

--- js/solutions/Problem4.js
+++ js/solutions/Problem4.js
@@ -5,18 +5,16 @@ exports.solve = function () {
 	
 	var util = require("../util"),
 		nums = util.range(100, 1000).reverse(),
-		cache = {};
+		max = -1;
 
-	// TODO: this algorithm doesn't work. 
-
-	var i,j;
-	for (i = 0; i < nums.length; i++) {
-		for (j = 0; j < nums.length; j++) {
-			if (util.isPalindrome('' + (nums[i] * nums[j]))) {
-				return nums[i] * nums[j];
+	nums.forEach(function (i) {
+		nums.forEach(function (j) {
+			var product = i * j;
+			if (util.isPalindrome('' + product) && product > max) {
+				max = product;
 			}
-		}
-	}
+		});
+	});
 	
-	return false;
+	return max;
 }
