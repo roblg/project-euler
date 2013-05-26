cc8d05c329d9cde5e769973ee709e286ac9aa8be
Robert Gay <robert.gay@redfin.com>
2013-05-25 20:14:17 -0700

    Updating Problem 16,20 to use BigInt class

---
+++ js/BigInt.js
@@ -0,0 +1,123 @@
+
+var digits = function (input /* [String|Number] */) {
+	if (typeof input === 'string') {
+		return input.split('').map(function (s) { return parseInt(s, 10); });
+	} else if (typeof input === 'number') {
+		var result = [];
+		do {
+			result.push(input % 10);
+			input = Math.floor(input / 10);
+		} while (input);
+		return result.reverse();
+	} else {
+		throw 'input must be String or Number';
+	}
+}
+
+var BigInt = function (input /* [Number|String|Array] */) {
+
+	if (typeof input === 'string' || typeof input === 'number') {
+		this._digits = digits(input);
+		if (this._digits.length === 0) {
+			this._digits.push(0);
+		}
+		// _digits are stored in reverse -- least significant place at 0 index
+		this._digits.reverse();
+	} else {
+		// if the input is an array, it's assume to already be in least-significant-first order
+		this._digits = input;
+	}
+}
+
+/**
+ * digits1 and digits2 are arrays of the digits of base-10 numbers,
+ * least significant digit first
+ */
+ var digitAdd = function (digits1, digits2) {
+	var temp, i = 0, carry = 0, result = [];
+	do {
+		temp = (digits1[i] || 0) + (digits2[i] || 0) + carry;
+		carry = Math.floor(temp / 10);
+		result.push(temp % 10);
+		i += 1;
+	} while (i < digits1.length1 || i < digits2.length);
+
+	while (carry) {
+		result.push(carry % 10);
+		carry = Math.floor(carry / 10);
+	}
+
+	return result;
+}
+
+BigInt.prototype.add = function (other) {
+	return new BigInt(digitAdd(this._digits, other._digits));
+}
+
+var digitMultiply = function (topDigits, bottomDigits) {
+	var resultDigits = [0];
+
+	var i, j, k, intermediate, carry;
+
+	// for each digit of the number *on bottom* in a traditional
+	// multiplication, we need to multiply it by every number in the top
+	// row, and resolve carries
+	for (j = 0; j < bottomDigits.length; j++) {
+		i = 0;
+		carry = 0;
+		intermediate = [];
+
+		for (k = j; k > 0; k--) {
+			// we need to shift over by 10 for every digit we've moved
+			// over in the bottom row
+			intermediate.push(0);
+		}
+
+		// for each digit in the top row, perform the multiplication
+		// and add in the carry value and add a result digit to intermediate
+		// output
+		do {
+			temp = (topDigits[i] * bottomDigits[j]) + carry;
+			carry = Math.floor(temp / 10);
+			intermediate.push(temp % 10);
+			i += 1;
+			// console.log(temp, carry, intermediate);
+		} while (i < topDigits.length);
+
+		// if we've run out of the digits in the top row, and we still
+		// have values to add, add them
+		// (note: we could probably do this by converting to a string, splitting,
+		// and then just using Array.apply() on the intermediate values)
+		while (carry) {
+			intermediate.push(carry % 10);
+			carry = Math.floor(carry / 10);
+		}
+
+		// add this bottom-row digit's contribution to the total
+		resultDigits = digitAdd(resultDigits, intermediate);
+
+	}
+
+	return resultDigits;
+}
+
+BigInt.prototype.multiply = function (other) {
+	var myDigits = this._digits;
+	var otherDigits = other._digits;
+
+	// the mutiplication algorithm in digitMultiply assumes that the first
+	// parameter is the top (longer) number. 
+	var myDigitsLonger = myDigits.length >= otherDigits.length,
+		topDigits = myDigitsLonger ? myDigits : otherDigits,
+		bottomDigits = myDigitsLonger ? otherDigits : myDigits;
+
+	return new BigInt(digitMultiply(topDigits, bottomDigits));
+}
+
+BigInt.prototype.toString = function () {
+	var reversedDigits = this._digits.slice().reverse();
+	return reversedDigits.join(''); // TODO: strip off leading 0s
+}
+
+module.exports = BigInt;
+
--- js/solutions/Problem16.js
+++ js/solutions/Problem16.js
@@ -1,32 +1,22 @@
 
-function multiplyByTwo(digits) {
-	var i, newVal, carry = 0;
-	for (i = 0; i < digits.length; i++) {
-		newVal = (digits[i] * 2) + carry;
-		carry = Math.floor(newVal / 10);
-		digits[i] = newVal % 10;
-	}
-	if (carry) {
-		digits.push(carry);
-	}
-}
 
 exports.solve = function () {
 
 	// I think the moral of this solution should be that there
 	// are some things Ruby/Python are way better at than JavaScript,
-	// hence the reason I wrote my own arbitrary-precision multiply-by-two
-	// routine...
+	// hence the reason I wrote my own arbitrary-precision BigInt class...
+
+	var BigInt = require("../BigInt");
 
 	var i,
-		digits = [2];
+		result = new BigInt(2);
 
 	// we've already added one of our 2s to the output list,
 	// so we only need to multiply it 999 more times. 
 	for (i = 0; i < 999; i++) {
-		multiplyByTwo(digits);
+		result = result.multiply(new BigInt(2));
 	}
 
-	return digits.reduce(function (prev, curr) { return prev + curr; });
+	return result._digits.reduce(function (prev, curr) { return prev + curr; });
 
 }
