35f078f685fe0eac2d4eed25492a50bbb5a66f56
Robert Gay <robert.gay@redfin.com>
2013-06-21 21:01:33 -0700

    Problem 74 JS solution

---
+++ js/solutions/Problem74.js
@@ -0,0 +1,38 @@
+
+// factorial cache
+// 0=>1, 1=>1, 2=>2, 3=>6, etc.
+var factCache = [1];
+for (var i = 1; i < 10; i++) {
+	factCache[i] = i * factCache[i-1];
+}
+
+exports.solve = function () {
+	
+	var util = require("../util");
+
+	var count = 0;
+	var chain, digits, temp;
+	for (var i = 1; i < 1000000; i++) {
+		console.log(i);
+		chain = [i];
+		temp = i;
+		while (true) {
+			digits = util.extractDigits(temp);
+			temp = util.sum( digits.map(function (d) { return factCache[d]; }) );
+			if (chain.indexOf(temp) === -1) {
+				chain.push(temp);
+				if (chain.length === 61) {
+					break;
+				}
+			} else {
+				break;
+			}
+		}
+
+		if (chain.length === 60) {
+			count += 1;
+		}
+	}
+
+	return count;
+}
